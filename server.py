e6fdeee70037e564cdffc885eb18388f0afcbcb2
PiyushCC <piyushchaudhari151001@gmail.com>
2023-02-06 11:03:40 +0530

    Upload and Download2

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, send_file, Response
+from flask import Flask, request, send_file, Response, send_from_directory
 from flask_cors import CORS
+import os
 import base64
 import json
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/imag', methods=['GET'])
+def myimg():
+    return send_file('save_pic.jpg', mimetype='image/jpg')
 
 @app.route('/success', methods=['POST'])
 def index():
@@ -13,22 +18,26 @@ def index():
     """
     # Read image from request and write to server's file system
     data = request.files['file']
-    print(request.form['plaintext'])
+    imagetext = request.form.get('plaintext')
+    print(imagetext)
     data.save('save_pic.jpg')
 
     # Do something with the image e.g. transform, crop, scale, computer vision detection
     # some_function_you_want()
 
     # Return the original/manipulated image with more optional data as JSON
-    saved_img = open('save_pic.jpg', 'rb').read() # Read as binary
-    saved_img_b64 = base64.b64encode(saved_img).decode('utf-8') # UTF-8 can be converted to JSON
-    response = {}
-    response['data'] = saved_img_b64
-    response['more_fields'] = 'more data' # Can return values such as Machine Learning accuracy or precision
+    # saved_img = open('save_pic.jpg', 'rb').read() # Read as binary
+    # saved_img_b64 = base64.b64encode(saved_img).decode('utf-8') # UTF-8 can be converted to JSON
+    # response = {}
+    # response['data'] = saved_img_b64
+    # print(saved_img_b64)
+    # response['more_fields'] = 'more data' # Can return values such as Machine Learning accuracy or precision
 
     # If only the image is required, you can use send_file instead
-    # return send_file('save_pic.jpg', mimetype='image/jpg') 
-    return Response(json.dumps(response))
+    return send_file('image.pdf', mimetype='application/pdf')
+
+    # For sending multiple use this
+    #return Response(json.dumps(response))
 
 if __name__=="__main__":
     app.run(debug=True)
